b30d1bbea2d5815d1b95469373a170654d1cc772
Satish <Satish@DESKTOP-NVDDM65>
2021-01-31 13:31:29 +0530

    first commit

---
+++ GitHubDemo2/src/main/java/logInPages/LoginPage.java
@@ -0,0 +1,9 @@
+package logInPages;
+
+public class LoginPage {
+	public void loginpages()
+	{
+		System.out.println("this is log in page");
+	}
+
+}
---
+++ GitHubDemo2/src/test/java/com/Testcases/Logintest.java
@@ -0,0 +1,11 @@
+package com.Testcases;
+
+public class Logintest {
+	
+	public void logintest()
+	{
+		System.out.println("this is log in test");
+		
+	}
+
+}
